1b5a6d9b5332bba6624ca272965ddd157bc92f65
kiyotech <kiyosawatech@gmail.com>
2023-01-30 13:11:35 +0900

    モデル、データベース作成

---
+++ db/migrate/20230130035902_create_resumes.rb
@@ -0,0 +1,11 @@
+class CreateResumes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :resumes do |t|
+      t.string     :title,       null: false
+      t.text       :resume_text, null: false
+      t.integer    :price,       null: false
+      t.references :user,        null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
