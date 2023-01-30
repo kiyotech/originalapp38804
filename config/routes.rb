96a288ff6123bbfaa34e68630c20b07fdb8d2e96
kiyotech <kiyosawatech@gmail.com>
2023-01-30 18:21:01 +0900

    アクション等記入

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'resumes/index'
   root to: "resumes#index"
   resources :users, only: [:index, :edit, :update]
-  resources :resumes, only: [:index, :new, :create]
+  resources :resumes, only: [:index, :new, :create, :show, :edit]
 end
