1cc008424c2bc6cd32123b61ee1e5bc501e18301
kiyotech <kiyosawatech@gmail.com>
2023-01-31 12:35:06 +0900

    編集削除機能の作成済

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,9 @@
 class ResumesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_resume, only: [:edit, :show, :update, :destroy]
   
   def index
-    @resumes = Resume.all
+    @resumes = Resume.all.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -10,19 +11,45 @@ class ResumesController < ApplicationController
   end
 
   def create
-    Resume.create(resume_params)
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    if !(@resume.user == current_user)
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @resume.update(resume_params)
+      redirect_to resume_path(@resume.id), method: :get
+    else
+      render :edit
+    end
   end
   
   def show
-    @resume = Resume.find(params[:id])
+  end
+
+  def destroy
+    if @resume.user == current_user
+      @resume.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
   def resume_params
     params.require(:resume).permit(:title, :image, :resume_text, :answer, :price).merge(user_id: current_user.id)
   end
-  
+  def set_resume
+    @resume = Resume.find(params[:id])
+  end
 end
