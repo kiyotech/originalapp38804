d0bdb06a09cbc43f5665aace46eebfceef785958
kiyotech <kiyosawatech@gmail.com>
2023-02-02 22:24:22 +0900

    PayJpに合わせた最低価格の設定

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -9,7 +9,7 @@ class Resume < ApplicationRecord
   end
   validates :price, presence: true
   validates :price,
-            numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999, message: 'は0円から9,999,999円の範囲で入力してください' }
+            numericality: { greater_than_or_equal_to: 50, less_than_or_equal_to: 9_999_999, message: 'は50円から9,999,999円の範囲で入力してください' }
   validates :price, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
 end
 
